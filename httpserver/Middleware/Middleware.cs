4a378f68435133d7f012fd65b27e42886981aaba
Softshag & Me <admin@softshag.dk>
2014-09-29 22:46:39 +0200

    Added middleware handler.

---
+++ httpserver/Middleware/Middleware.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocketServer
+{
+
+	public delegate void MiddlewareHandler(HTTPRequest request, HTTPResponse response);
+
+	public class Middleware
+	{
+		private IList<MiddlewareHandler> _handlers;
+
+		public Middleware ()
+		{
+			_handlers = new List<MiddlewareHandler> ();
+		}
+
+		public void Use(MiddlewareHandler handler) {
+			_handlers.Add (handler);
+		}
+
+		public void Run(HTTPRequest request, HTTPResponse response) {
+
+			foreach (var handler in _handlers) {
+				handler(request, response);
+			}
+		}
+	}
+}
+
